7d44692ca76ff27fb633903e321782228bd3ebc5
Jordan Rios <145077985+Jordan130@users.noreply.github.com>
2024-02-12 16:14:55 -0500

    Update blog.js

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,9 +1,9 @@
 // Controller for the Blog List page
 exports.blogList = (req, res) => {
-  res.render('blogList', { title: 'Blog List', message: 'Under Construction' });
+  res.render('blogList', { title: 'Blog List' });
 };
 
 // Controller for the Blog Add page
 exports.blogAdd = (req, res) => {
-  res.render('blogAdd', { title: 'Add Blog', message: 'Under Construction' });
+  res.render('blogAdd', { title: 'Add Blog' });
 };
